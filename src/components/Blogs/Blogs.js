381a21f5cc8264ebe719fabdd4faabffa84aafac
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-04-04 22:24:23 +0600

    fix router reload issue

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -4,18 +4,12 @@ import { Accordion } from 'react-bootstrap';
 
 const Blogs = () => {
     return (
-        <div className='w-75 m-5 p-5 shadow-lg p-3 mb-5 bg-body rounded'>
+        <div className='w-75 m-5 shadow-lg p-5 bg-body rounded'>
            <Accordion> <h1>Question and Answer</h1>
   <Accordion.Item eventKey="0">
-    <Accordion.Header>Accordion Item #1</Accordion.Header>
+    <Accordion.Header>Question : What is a purpose of context API</Accordion.Header>
     <Accordion.Body>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
+      <span className='fw-bold'>Answer :</span>
     </Accordion.Body>
   </Accordion.Item>
   <Accordion.Item eventKey="1">
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 const Header = () => {
     return (
-        <nav className='fw-bold fs-3'>
-            <Link to='/home'>HOME</Link>
-            <Link to='/reviews'>REVIEWS</Link>
-            <Link to='/dashboard'>DASHBOARD</Link>
-            <Link to='/blogs'>BLOGS</Link>
-            <Link to='/about'>ABOUT</Link>
+        <nav className='fw-bold fs-5 pt-3'>
+            <Link className='text-decoration-none text-black p-4' to='/home'>HOME</Link>
+            <Link className='text-decoration-none text-black p-4' to='/reviews'>REVIEWS</Link>
+            <Link className='text-decoration-none text-black p-4' to='/dashboard'>DASHBOARD</Link>
+            <Link className='text-decoration-none text-black p-4' to='/blogs'>BLOGS</Link>
+            <Link className='text-decoration-none text-black p-4' to='/about'>ABOUT</Link>
         </nav>
 
     );
